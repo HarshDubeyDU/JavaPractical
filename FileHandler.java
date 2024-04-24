02533a040be64ad5b5fa6f2430bfa0a03c11fcfb
Harsh Dubey <87745474+HarshDubeyDU@users.noreply.github.com>
2024-04-24 12:11:43 +0530

    Practical till 7

---
+++ FileHandler.java
@@ -0,0 +1,31 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Scanner;
+
+public class FileHandler {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("File 1:");
+        String fileOneName = sc.next();
+        System.out.print("File 2:");
+        String fileTwoName = sc.next();
+        try {
+            FileReader fileOne = new FileReader(fileOneName);
+            File fileTwo = new File(fileTwoName);
+            BufferedReader fileReader = new BufferedReader(fileOne);
+            PrintWriter fileWriter = new PrintWriter(fileTwo);
+            String fileContent;
+            while ((fileContent = fileReader.readLine()) != null) {
+                fileWriter.println(fileContent);
+            }
+            fileOne.close();
+            fileWriter.close();
+        } catch (IOException e) {
+            System.out.println(fileOneName + " was not found.");
+        }
+        sc.close();
+    }
+}
---
+++ FilePrinter.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class FilePrinter {
+    public static void main(String[] args) {
+        try (FileReader fileReader = new FileReader("Hello.txt")) {
+            BufferedReader reader = new BufferedReader(fileReader);
+            PrintWriter out = new PrintWriter(System.out);
+            String fileContent;
+            while ((fileContent = reader.readLine()) != null && fileContent.startsWith("//")) {
+                out.println(fileContent);
+            }
+            reader.close();
+            out.close();
+        } catch (IOException e) {
+
+        }
+    }
+}
